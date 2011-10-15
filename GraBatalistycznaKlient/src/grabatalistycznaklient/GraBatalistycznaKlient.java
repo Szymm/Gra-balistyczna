1eb448a312d2e3e0dd24c190319db4691c3e4ce2
Szymon <szymm.j@gmail.com>
2011-10-15 20:43:02 +0200

    przetworzone

---
+++ GraBatalistycznaKlient/src/grabatalistycznaklient/GraBatalistycznaKlient.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grabatalistycznaklient;
+
+/**
+ *
+ * @author szymi
+ */
+public class GraBatalistycznaKlient {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
---
+++ GraBatalistycznaSerwer/src/grabatalistycznaserwer/GraBatalistycznaSerwer.java
@@ -0,0 +1,8 @@
+package grabatalistycznaserwer;
+
+public class GraBatalistycznaSerwer {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
